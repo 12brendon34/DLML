31164fc567e0e67354204b13b340c94b1d934c51
Brendon <43822289+12brendon34@users.noreply.github.com>
2024-05-02 00:32:20 -0700

    Simple DL2 Log Goin

--- Hooks.cpp
+++ Hooks.cpp
@@ -39,23 +39,52 @@ void HookFunction(LPVOID target, LPVOID destination, LPVOID* original) {
 	}
 }
 
-typedef bool(*add_source)(char const* Path, int FFSAddSourceFlags);
-add_source Add_Source_Real;
-bool Add_Source(char const* Path, int FFSAddSourceFlags) {
-	return Add_Source_Real(Path, FFSAddSourceFlags);
+typedef int* (*logsettingsinstance)();
+logsettingsinstance LogSettingsInstance;
+int* LogSettingsInstance_Hook() {
+	int* logint = LogSettingsInstance();
+	*logint = INT_MAX;
+	return logint;
 }
 
-typedef void(__cdecl* initializegamescript)(LPCSTR locale);
-initializegamescript InitializeGameScript_Real;
-void InitializeGameScript(LPCSTR locale) {
-	for (size_t i = 0; i < ModInfoList.size(); i++)
-		Add_Source(ModInfoList[i].ModPath.c_str(), 9);
-
-	return InitializeGameScript_Real(locale);
+typedef int(*getcategorylevel)(int This, char* Catagory);
+getcategorylevel GetCategoryLevel;
+int GetCategoryLevel_Hook(int This, char* Catagory) { //GetCategoryLevel
+	return INT_MAX;
 }
 
-FARPROC InitializeGameScript_Address;
-FARPROC Add_Source_Address;
+typedef void(__cdecl* _clogv)(int LogType, char* thread, char* sourcefile, int linenumber, int CLFilterAction, int CLLineMode, char const* __ptr64 message, char const* __ptr64 printarg);
+_clogv CLogV = nullptr;
+void CLogV_Hook(int logtype, char* thread, char* sourcefile, int linenumber, int CLFilterAction, int CLLineMode, char const* __ptr64 message, char const* __ptr64 printarg) {
+	std::string Message;
+
+	if (logtype == 5)
+		Message.append(" DBUG :");
+	else if (logtype == 4)
+		Message.append(" WARN :");
+	else if (logtype == 3)
+		Message.append(" INFO :");
+	else if (logtype == 2)
+		Message.append(" ERRR :");
+
+	Message.append(" [" + (std::string)thread + "] ");
+
+	if (CLFilterAction == 2)
+		Message.append("| ");
+	else
+		Message.append("> ");
+
+	char Buffer[65528];
+	vsprintf(Buffer, message, (va_list)printarg);
+	Message.append(Buffer);
+
+	(void)dbgprintf("%s", Message.c_str());
+	return CLogV(logtype, thread, sourcefile, linenumber, CLFilterAction, CLLineMode, message, printarg);
+}
+
+FARPROC LogSettingsInstance_Address;
+FARPROC GetCategoryLevel_Address;
+FARPROC CLogV_Address;
 
 BOOL CreateHooks(HMODULE hmodule) {
 
@@ -65,17 +94,18 @@ BOOL CreateHooks(HMODULE hmodule) {
 	HMODULE EngineDll = GetModuleHandleSimple("engine_x64_rwdi.dll");
 	HMODULE FilesystemDll = GetModuleHandleSimple("filesystem_x64_rwdi.dll");
 
-	InitializeGameScript_Address = GetProcAddressSimple(EngineDll, "InitializeGameScript");
-	Add_Source_Address = GetProcAddressSimple(FilesystemDll, "?add_source@fs@@YA_NPEBDW4ENUM@FFSAddSourceFlags@@@Z");
-
 	//dl2 InitializeGameScript is now LoadGameScriptDll and is not exported
 	//InitializeGameScript_Address = (FARPROC)((DWORD_PTR*)EngineDll + 0x7ec400 / (2 * sizeof(DWORD)));
 
 	//dl2 fs::add_source -> fs::mount
-	//Add_Source_Address = GetProcAddressSimple(FilesystemDll, "?mount@fs@@YA_NAEBUmount_path@1@GPEAPEAVCFsMount@@@Z");
 
-	HookFunction(InitializeGameScript_Address, &InitializeGameScript, reinterpret_cast<void**>(&InitializeGameScript_Real));
-	HookFunction(Add_Source_Address, &Add_Source, reinterpret_cast<void**>(&Add_Source_Real));
+	LogSettingsInstance_Address = GetProcAddress(FilesystemDll, "?Instance@Settings@Log@@SAAEAV12@XZ");
+	GetCategoryLevel_Address = GetProcAddress(FilesystemDll, "?GetCategoryLevel@Settings@Log@@QEBA?AW4TYPE@ELevel@2@PEBD@Z");
+	CLogV_Address = GetProcAddress(FilesystemDll, "?_CLogV@@YAXW4TYPE@ELevel@Log@@PEBD1HW4ENUM@CLFilterAction@@W44CLLineMode@@1PEAD@Z");
+
+	HookFunction(LogSettingsInstance_Address, &LogSettingsInstance_Hook, reinterpret_cast<void**>(&LogSettingsInstance));
+	HookFunction(GetCategoryLevel_Address, &GetCategoryLevel_Hook, reinterpret_cast<void**>(&GetCategoryLevel));
+	HookFunction(CLogV_Address, &CLogV_Hook, reinterpret_cast<void**>(&CLogV));
 
 	(void)MH_EnableHook(MH_ALL_HOOKS);
 
